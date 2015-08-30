b98bb259c68b3f7ebb6d0d51c7ff5741806bb305
Andre-487 <andrey.prokopyuk@gmail.com>
2015-08-30 18:11:55 +0300

    Fix arrays and add path support

--- reporter/type-text.js
+++ reporter/type-text.js
@@ -17,7 +17,9 @@ module.exports = function textReporter(validationData, options) {
 
     function mapItem(item) {
         return [
-            'At line: ' + item.line + ', column: ' + item.column,
+            item.line ?
+                'At line: ' + item.line + ', column: ' + item.column :
+                'At path: ' + item.path,
             item.type,
             item.text
         ].join('\t');
@@ -34,17 +36,17 @@ module.exports = function textReporter(validationData, options) {
         '',
         validationData.errors.length ? [
             prepareString('Errors:', 'red'),
-            _.map(validationData.errors, mapItem),
+            _.map(validationData.errors, mapItem).join('\n'),
             ''
         ] : null,
         validationData.warnings.length ? [
             prepareString('Warnings:', 'blue'),
-            _.map(validationData.warnings, mapItem),
+            _.map(validationData.warnings, mapItem).join('\n'),
             ''
         ] : null,
         validationData.info.length ? [
             prepareString('Info:', 'yellow'),
-            _.map(validationData.info, mapItem),
+            _.map(validationData.info, mapItem).join('\n'),
             ''
         ] : null,
         validationData.isValid ? prepareString('All correct', 'green') : null

69ef3a763b9d947d37118dc86bec870f319a25a0
Andre-487 <andrey.prokopyuk@gmail.com>
2015-08-30 18:33:10 +0300

    Implement plugin for HTTPS validation

--- examples/config.yandex.js
+++ examples/config.yandex.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 /**
  * @file
  * Config example for Yandex opensearch.xml file
@@ -10,5 +12,31 @@ module.exports = {
         {level: 'warning', type: 'ShouldIncludeExample'}
     ],
     plugins: [
+        /**
+         * Check HTTPS in urls
+         * @param {Object} feedJson
+         */
+        function checkHttps(feedJson) {
+            var path = 'OpenSearchDescription.Url';
+            var urls = _.get(feedJson, path);
+
+            var errors = [];
+            if (!urls) {
+                errors.push({level: 'error', path: path, text: 'No urls'});
+            }
+
+            _.each(urls, function (item, i) {
+                var url = _.get(item, '$.template');
+                var type = _.get(item, '$.type');
+
+                var errPath = [path, i, '$.template'].join('.');
+                if (!url) {
+                    errors.push({level: 'error', path: errPath, text: 'No url template for type ' + type});
+                } else if (!/(https:)?\/\//.test(url)) {
+                    errors.push({level: 'error', path: errPath, text: 'Non HTTPS schema in type ' + type});
+                }
+            });
+            return errors;
+        }
     ]
 };
